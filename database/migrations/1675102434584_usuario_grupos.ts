5c688e203eba0e78eb6484edd98e22d87d341787
Carlos Sandoval <csandovalc@unal.edu.co>
2023-02-14 16:53:06 -0500

    Fix: Migrations overwrite the index generating errors

--- database/migrations/1675102434584_usuario_grupos.ts
+++ database/migrations/1675102434584_usuario_grupos.ts
@@ -5,7 +5,7 @@ export default class UsuarioGrupos extends BaseSchema {
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.integer('codigo_usuario').unsigned().index('codigo_usuario')
+      table.integer('codigo_usuario').unsigned()
       table.integer('codigo_grupo').unsigned().index('codigo_grupo')
       table.date('fecha_inicio').notNullable()
       table.foreign('codigo_usuario').references('usuarios.codigo_usuario').onDelete('cascade')
